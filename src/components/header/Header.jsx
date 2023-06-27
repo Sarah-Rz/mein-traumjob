274b6b4575bb7a6bddc2a05dfce50014dc425eaf
Sarah_RZ <rezaeisara1994@yahoo.com>
2023-06-28 03:14:17 +0330

    header changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,9 +12,6 @@ const Header = () => {
         <p className="text-sub">Hier erfahren Sie mehr über meinen Werdegang und die Herausforderungen auf dem Weg zu meinem Traumjob als Programmiererin!</p>
         <p className="text-sub-1">Mein Motto ist:</p><p className="text-sub"> Übung macht den Meister. und alles ist möglich, wenn man es nur will!</p>
        <br />
-       <div className="m1-img">
-          <img src={m1} alt="" />
-        </div>
         <h3 className="text-light">Ich bin Sara </h3>
         <h2 className="text-primary">Web Entwicklerin</h2>
         <CTA />
